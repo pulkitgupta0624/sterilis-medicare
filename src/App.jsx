17755313fafae33e5f5bf65cf4ab11d6695a95b9
Pulkit gupta <122692240+pulkitgupta0624@users.noreply.github.com>
2024-06-16 22:51:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home/Home";
-import About from "./components/About/About.jsx";
-import Testimonial from "./components/Testimonial/Testimonial.jsx";
-import Product from "./components/Product/Product.jsx";
-import Contact from "./components/Contact/Contact.jsx";
-import Footer from "./components/Footer/Footer.jsx";
+import Home from "./components/Home/Home.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -14,12 +8,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/testimonial" element={<Testimonial />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
-        <Footer />
       </div>
     </Router>
   );
