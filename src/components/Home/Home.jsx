d11315110a17e60d903d264dbcb02520cdd12d47
Pulkit gupta <122692240+pulkitgupta0624@users.noreply.github.com>
2024-06-16 22:52:55 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import BannerBackground from "../../Assets/image4.jpg";
-import Navbar from "../Navbar/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 import { FiArrowRight } from "react-icons/fi";
+import Testimonial from "../Testimonial/Testimonial";
+import Footer from "../Footer/Footer";
+import AboutBackgroundImage from "../../Assets/image1.jpg";
 
 const Home = () => {
   return (
@@ -16,7 +19,14 @@ const Home = () => {
             STERILIS MEDICARE
           </h1>
           <p className="primary-text">
-            Here will be a description about website
+              <p className="xyz">Strength</p>
+              <p className="xyz">Together</p>
+              <p className="xyz">Experience</p>
+              <p className="xyz">Relationship</p>
+              <p className="xyz">Investment</p>
+              <p className="xyz">Logistics</p>
+              <p className="xyz">Improve</p>
+              <p className="xyz">Subcontracting</p>             
           </p>
           <button className="secondary-button">
             Visit us <FiArrowRight />{" "}
@@ -26,6 +36,42 @@ const Home = () => {
           <img src={BannerImage} alt="" />
         </div> */}
       </div>
+      <div className="about-section-container">
+     
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+          The slogan – “Healing is our goal”
+        </h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <p className="primary-text">
+          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
+          facilisis at fringilla quam.
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h1>HERE WILL WE PRODUCT INFORMATION FOR HOME PAGE</h1>
+    </div>
+      <Testimonial/>
+      <div className="contact-page-wrapper">
+      <h1 className="primary-heading">Have Question In Mind?</h1>
+      <h1 className="primary-heading">Let Us Help You</h1>
+      <div className="contact-form-container">
+        <input type="text" placeholder="yourmail@gmail.com" />
+        <button className="secondary-button">Submit</button>
+      </div>
+    </div>
+      <Footer/>
     </div>
   );
 };
