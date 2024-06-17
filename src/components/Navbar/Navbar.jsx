439103abe3f7b5dce2abbfb191ee23e6f13e0cb9
Pulkit gupta <122692240+pulkitgupta0624@users.noreply.github.com>
2024-06-17 13:17:36 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,9 +13,12 @@ import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import { useNavigate } from "react-router-dom";
+import Logo from "../../Assets/logo.jpg"
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
+  const navigate = useNavigate();
   const menuOptions = [
     {
       text: "Home",
@@ -41,15 +44,15 @@ const Navbar = () => {
   return (
     <nav>
       <div className="nav-logo-container">
-        <h1>.LOGO</h1>
+        <img src={Logo} alt="" />
       </div>
       <div className="navbar-links-container">
         <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/products">Products</a>
         <a href="/contact">Contact</a>
-        <button className="primary-button">Sign in</button>
-        <button className="primary-button">Create account</button>
+        <button className="primary-button" onClick={() => navigate('/login')}>Sign in</button>
+        <button className="primary-button" onClick={() => navigate('/signup')}>Create account</button>
       </div>
       <div className="navbar-menu-container">
         <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
